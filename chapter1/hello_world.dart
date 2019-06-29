4aa97cde7c0eb57bcc2f2fb7aedc9d95c4033eba
liqilong <liqilong199126@gmail.com>
2019-06-29 19:18:05 +0800

    Dart变量与常量

---
+++ chapter1/hello_world.dart
@@ -0,0 +1,5 @@
+// main方法世程序入口
+void main() {
+  // print方法可以再控制台输出内容
+  print("Hello World!");
+}
---
+++ chapter2/var_and_const.dart
@@ -0,0 +1,20 @@
+void main() {
+  var a;
+  print(a);
+
+  a = 10;
+  print(a);
+
+  a = 'Hello Dart';
+  print(a);
+
+  var b = 20;
+  print(b);
+
+  // 使用final变量一个只能被赋值一次的变量
+  final c = 30;
+  // c = 50;
+
+  const d = 20;
+  // d = 50;
+}
